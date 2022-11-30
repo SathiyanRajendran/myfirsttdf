cc6aef7f16d5428a56c14378651a1663a493a8c7
Sathiyan R <sathiyan@whiteblue.in>
2022-11-30 14:39:58 +0530

    template driven form commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,43 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'myfirsttdf';
+
+  @ViewChild('userForm') uf:NgForm | undefined;
+  countryList=[
+    {"countryname":"India"},
+    {"countryname":"USA"},
+    {"countryname":"England"}
+  ];
+  SetDefaultValue(){
+    // this.uf?.setValue({
+    //   personalinformation:{
+    //     firstname:'muthu',
+    //     lastname:'journey',
+    //     email:'abc@gmail.com',
+    //     gender:'male',
+    //     isMarried:true
+    //         },
+    //         address:{
+    //           country:'USA',
+    //           city:"newyork",
+    //           street:'wall mart',
+    //           pincode:614203
+    //         }
+    // }) ;
+    this.uf?.form.patchValue({
+      personalinformation:{
+       firstname:'Muthu Journey'
+      }
+    });
+  }
+  // onSubmit(myForm:NgForm)
+  // {
+  //   console.log(myForm);
+  // }
+
+  onSubmit()
+  {
+    console.log(this.uf)
+    this.uf?.reset();
+  }
 }
